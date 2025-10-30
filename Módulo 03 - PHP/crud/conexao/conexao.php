dcad12217470dfdc08d0c8c936b6ac07ed6701f8
CamilaCalve <145620266+CamilaCalve@users.noreply.github.com>
2025-10-30 10:57:58 -0300

    .

---
+++ Módulo 03 - PHP/crud/conexao/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+    $servername = "localhost"; // ip ou dominio do server
+    $username = "root"; // usuário no banco de dados 
+    $password = ""; //senha do usuário no banco de dados
+    $dbname = "faculdade";
+
+    //criar conexão com o banco
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //verificar conexão
+    if ($conn->connect_error){
+        die("Conexão falhou"); //parar o programa
+    }
+
+
+?>
